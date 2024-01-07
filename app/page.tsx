f534d45801222ec768dae2e7cb1d3a5c91ff856f
Nitesh Agnihotri <niteshagni1830@gmail.com>
2024-01-07 20:29:19 +0530

    added gototop

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import ExperienceSection from "@/components/experience";
 import IntroSection from "@/components/intro";
 import ProjectsSection from "@/components/projects";
 import SkillsSection from "@/components/skills";
+import ScrollToTop from "react-scroll-to-top";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <EducationSection />
       <ContactForm />
       <h1 className="text-sm mb-5 flex text-center px-5 justify-center gap-y-5">Designed & Developed By Nitesh Agnihotri</h1>
+      <ScrollToTop className="flex items-center justify-center" width="22" color="#374151"/>
     </main>
   )
 }
